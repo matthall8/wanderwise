1e6ae32180817636d904f4a2acebcc81ce121004
Matt Hall <matthall8@gmail.com>
2021-08-06 07:43:20 +0100

    Added linting and implemented recommendations to create _documt.js

--- pages/mexico-city-to-oaxaca.js
+++ pages/mexico-city-to-oaxaca.js
@@ -10,10 +10,6 @@ const Oaxaca = () => {
     return (
         <React.Fragment>
             <Head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Muli:wght@500;700;800;900&display=swap" rel="stylesheet" /> 
                 <title>Mexico City To Oaxaca - Wander Wise</title>
             </Head>
             <StyledBody />
