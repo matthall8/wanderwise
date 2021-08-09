27efd3634bfe7adc255d1d1ff6a69aebd3be5318
Matt Hall <matthall8@gmail.com>
2021-08-09 08:37:35 +0100

    Added What is Wander Wise content to the homepage

--- src/sections/WhatIsWanderWise/WhatIsWanderWise.js
+++ src/sections/WhatIsWanderWise/WhatIsWanderWise.js
@@ -10,10 +10,13 @@ const WhatIsWanderWise= () => {
         What Is Wander Wise?
       </Heading>
       <TextContent>
-        This is lorum ipsum text content which is sat here to be a placeholder for the loook and feel of the text content
+        I’ll never forget the week I arrived in Bogota, to begin my first backpacking trip. The excitement, the nerves and then the confusion. How do I get to my next destination? How long will it take? Do I fly or take the bus? Are there trains? How much will it cost? Is it safe?  Is this the best way? The seeds for Wander Wise were sown.
       </TextContent>
       <TextContent>
-        This is a continuation of lorum ipsum text content which is sat here to be a placeholder for the loook and feel of the text content
+        Wander Wise is a multi-transport search tool that helps you make the best decision on how to get from A to B. It answers all the questions I had when arriving in Colombia. 
+      </TextContent>
+      <TextContent>
+        We provide information on only the safest transportation companies and help you book directly, with their best price guaranteed. 
       </TextContent>
     </StyledWhatIsSection>
   );
--- src/sections/WhatIsWanderWise/WhatIsWanderWise.style.js
+++ src/sections/WhatIsWanderWise/WhatIsWanderWise.style.js
@@ -3,5 +3,7 @@ import styled from "styled-components";
 export const StyledWhatIsSection = styled.section` {
   padding: 28px 16px 12px;
   color: #000;
+  width: 80%;
+  margin: 0 auto;
 }
 `;
