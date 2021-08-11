40c1905af07b70e4633a35b93994c2585564d18a
Matt Hall <matthall8@gmail.com>
2021-08-11 08:50:07 +0100

    Moved viewport from _document.js to _app.js due to NEXT JS warning

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
             <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet" />
             <link href="https://fonts.googleapis.com/css2?family=Muli:wght@500;700;800;900&display=swap" rel="stylesheet" />         
         </Head>
