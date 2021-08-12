a10cdd744806a34227f428d1c90e81fb8543d2d7
Matt Hall <matthall8@gmail.com>
2021-08-12 07:38:52 +0100

    Made the width 100% with padding on mobile

--- src/components/TextContent/TextContent.style.js
+++ src/components/TextContent/TextContent.style.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const StyledText = styled.p` {
+  @media (min-width:641px) {
+    width: 40em;
+  }
     font-size: 1rem;
     font-family: 'Muli', sans-serif;
     padding: 12px 16px;
     margin: 0px auto;
     text-align: left;
     line-height: 1.75;
-    width: 40em;
     font-weight: normal;
   }
   `;
