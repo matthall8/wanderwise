f1ad6dce797a7d79aed92d32ca51153ae8a672e8
Matthew Hall <matthewhall@Matthews-MacBook-Air.local>
2022-08-01 09:14:38 +0100

    Sanity set up and basic blog post functionality added to the website

---
+++ pages/blog/[slug].js
@@ -0,0 +1,35 @@
+import client from "../../client";
+
+const Post = ({post}) => {
+  
+  return (
+    <article>
+      <h1>{post?.slug?.current}</h1>
+    </article>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  const { slug = "" } = context.params
+  const post = await client.fetch(`
+    *[_type == "post" && slug.current == $slug][0]
+  `, { slug })
+  return {
+    props: {
+      post
+    }
+  }
+}
+
+export default Post
