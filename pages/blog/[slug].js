09d21168bd93c9c05676d1e28b593d60049ae29b
Matthew Hall <matthewhall@Matthews-MacBook-Air.local>
2022-08-02 14:29:49 +0100

    Blogs display with page title and body copy along withi blog homepage

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,17 +1,35 @@
 import client from "../../client";
 
+import groq from 'groq'
+import {PortableText} from '@portabletext/react'
+
 const Post = ({post}) => {
-  
+  const {
+     title = 'Missing title', 
+     name = 'Missing name',
+     body = []
+    } = post
   return (
     <article>
-      <h1>{post?.slug?.current}</h1>
+      <h1>{title}</h1>
+      <PortableText
+        value={body}
+      />
     </article>
   )
 }
 
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  body
+}`
+
 export async function getStaticPaths() {
   const paths = await client.fetch(
-    `*[_type == "post" && defined(slug.current)][].slug.current`
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
   )
 
   return {
@@ -22,9 +40,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const { slug = "" } = context.params
-  const post = await client.fetch(`
-    *[_type == "post" && slug.current == $slug][0]
-  `, { slug })
+  const post = await client.fetch(query, { slug })
   return {
     props: {
       post
