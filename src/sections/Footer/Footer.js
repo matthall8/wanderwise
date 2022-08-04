c11148beac35ec34cbf0cdd75fe5b97b3df05b62
Matthew Hall <matthewhall@Matthews-MacBook-Air.local>
2022-08-04 11:55:21 +0100

    Updating footer to show correct year

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <StyledFooter>
        <p>Wanderwise is not responsible for content on external Web sites.</p>
-       <p>&copy; 2021 Wanderwise. All rights reserved. </p>
+       <p>&copy; 2022 Wanderwise. All rights reserved. </p>
     </StyledFooter>
   );
 };
