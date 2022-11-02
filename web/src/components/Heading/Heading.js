667f6aa403d9211d0500f5aee593bdbac3ffb586
Matthew Hall <matthewhall@Matthews-MacBook-Air.local>
2022-11-02 09:15:59 +0000

    Updated header tag to H1 on blog

--- web/src/components/Heading/Heading.js
+++ web/src/components/Heading/Heading.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {StyledHeader, StyledLightHeader, StyledH3Header} from "./Heading.style";
+import {StyledHeader, StyledLightHeader, StyledH1Header, StyledH3Header} from "./Heading.style";
 
 const Header = (props) => {
   if (props.type === 'light') {    
@@ -15,6 +15,13 @@ if (props.type === 'h3') {
     <StyledH3Header> 
       {props.children}
     </StyledH3Header>
+  )
+}
+if (props.type === 'h1') {    
+  return  (
+    <StyledH1Header> 
+      {props.children}
+    </StyledH1Header>
   )
 }
 return  (
