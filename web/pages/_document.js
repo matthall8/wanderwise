41f9d97e0d672ceea5d46f3e40af839f63e9a9e0
Matthew Hall <matthall8@gmail.com>
2025-01-30 14:49:38 +0000

    Added tags to <head>

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -12,7 +12,20 @@ class MyDocument extends Document {
         <Head>
             <meta charSet="UTF-8" />
             <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Muli:wght@500;700;800;900&display=swap" rel="stylesheet" />         
+            <link href="https://fonts.googleapis.com/css2?family=Muli:wght@500;700;800;900&display=swap" rel="stylesheet" /> 
+            <Script
+              id="gtm-script"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                  __html: `
+                      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-KGVHFFBQ');
+                  `,
+              }}
+          />        
         </Head>
         <body>
           <noscript>
